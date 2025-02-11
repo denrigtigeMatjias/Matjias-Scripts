42585f9a56f882e6f4071738802a8c5fb1a545f8
Matjias <117152858+denrigtigeMatjias@users.noreply.github.com>
2025-02-12 00:58:31 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,11 @@
 local PlaceIds = {
     ["Mining Go"] = {id = 116434053579571, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Mining%20GO"},
     ["Fisch"] = {id = 16732694052, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Fisch.lua"},
-    ["Backrooms Drift"] = {id = 138365924124161, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Backroom%20Drifts.lua"},
+    ["Backrooms Drift"] = {id = 138365924124161, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/NO%20UPDATE/Backroom%20Drifts.lua"},
     ["Arsenal"] = {id = 286090429, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Arsenal.lua"},
     ["Miners Haven"] = {id = 258258996, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Miners%20Haven.lua"},
-    ["Egg Tree Incremental"] = {id = 15055025587, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Egg%20Tree%20Incemental.lua"},
-    ["Universal GUI"] = {id = 0, url = ""},
+    ["Egg Tree Incremental"] = {id = 15055025587, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/NO%20UPDATE/Egg%20Tree%20Incemental.lua"},
+    ["Universal GUI"] = {id = 0, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Universal%20GUI.lua"},
 }
 
 local found = false
