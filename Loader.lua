a972dac489656f3491c125979484b5bbd9e1d333
Matjias <117152858+denrigtigeMatjias@users.noreply.github.com>
2025-02-12 01:08:14 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,13 @@
 local PlaceIds = {
-    ["Mining Go"] = {id = 116434053579571, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Mining%20GO"},
-    ["Fisch"] = {id = 16732694052, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Fisch.lua"},
-    ["Backrooms Drift"] = {id = 138365924124161, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/NO%20UPDATE/Backroom%20Drifts.lua"},
     ["Arsenal"] = {id = 286090429, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Arsenal.lua"},
+    ["Fisch"] = {id = 16732694052, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Fisch.lua"},
     ["Miners Haven"] = {id = 258258996, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Miners%20Haven.lua"},
-    ["Egg Tree Incremental"] = {id = 15055025587, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/NO%20UPDATE/Egg%20Tree%20Incemental.lua"},
+    ["Mining Go"] = {id = 116434053579571, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Mining%20GO"},
     ["Universal GUI"] = {id = 0, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/Universal%20GUI.lua"},
+    
+    -- Won't Receive Any Updates
+    ["Backrooms Drift"] = {id = 138365924124161, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/NO%20UPDATE/Backroom%20Drifts.lua"},
+    ["Egg Tree Incremental"] = {id = 15055025587, url = "https://raw.githubusercontent.com/denrigtigeMatjias/Matjias-Scripts/refs/heads/main/NO%20UPDATE/Egg%20Tree%20Incemental.lua"},
 }
 
 local found = false
